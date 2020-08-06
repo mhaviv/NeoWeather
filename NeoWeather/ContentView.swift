9e2973e1de74ae870a83ad7d70254e171b7243e3
Michael <mhaviv18@gmail.com>
2020-08-05 23:23:05 -0400

    Initial Commit

---
+++ NeoWeather/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NeoWeather
+//
+//  Created by Michael Haviv on 8/5/20.
+//  Copyright © 2020 Michael Haviv. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
